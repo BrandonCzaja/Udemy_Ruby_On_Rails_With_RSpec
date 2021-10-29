51e369a0230ce1068c5d853c17f469f10d765f2f
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-10-29 18:29:21 -0500

    Added validation requirements to Words Model. Added specs to check validation of Word Model

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Word, type: :model do
     it {is_expected.to have_db_column(:value)}
     it {is_expected.to have_db_column(:language)}
   end
+
+  # Added because of the validation in models/word.rb
+  describe 'validations' do
+    it {is_expected.to validate_presence_of(:value)}
+    it {is_expected.to validate_presence_of(:language)}
+  end
 end
